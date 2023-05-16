af80361062622b29916f788d8b18c7f04012557b
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-16 10:52:18 +0800

    structure

---
+++ models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
+
+db = SQLAlchemy() # Our DB Handler
+
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   db.Sequence('posts_id_seq'),
+                   primary_key=True)
+    
+    profile_id = db.Column(db.Integer,
+                           db.ForeignKey('profiles.id'),
+                           nullable=False)
+    
+    timestamp = db.Column(db.DateTime,
+                          server_default=db.func.now(),
+                          onupdate=db.func.now())
+    
+    text = db.Column(db.Text())
+
+
+    def __init__(self, text):
+        self.text = text
+
+    # For profile class
+    # posts = db.relationship('Posts',
+    #                           backref='profiles',
+    #                           lazy=True)
