4268a29c62388d1363cf7e9ef3a3fb454298812b
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-21 12:33:18 +0800

    organised models file

--- database/models.py
+++ database/models.py
@@ -6,25 +6,18 @@ db = SQLAlchemy() # Our DB Handler
 class Posts(db.Model):
     __tablename__ = 'posts'
 
-    id = db.Column(db.Integer,
-                   db.Sequence('posts_id_seq'),
-                   primary_key=True)
-    
-    # profile_id = db.Column(db.Integer,
-    #                        db.ForeignKey('profiles.id'),
-    #                        nullable=False)
-    
-    timestamp = db.Column(db.DateTime,
-                          server_default=db.func.now(),
-                          onupdate=db.func.now())
-    
+    id = db.Column(db.Integer,db.Sequence('posts_id_seq'),primary_key=True)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
     text = db.Column(db.Text())
-
 
     def __init__(self, text):
         self.text = text
 
-    # For profile class
-    # posts = db.relationship('Posts',
-    #                           backref='profiles',
-    #                           lazy=True)
+
+class Profiles(db.Model):
+
+    __tablename__ = 'profiles'
+
+    id = db.Column(db.Integer,db.Sequence('profiles_id_seq'),primary_key=True)
+    posts = db.relationship('Posts', backref='profiles', lazy=True)
