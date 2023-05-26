041a4ee0187e7738c855af762a4e737d0e9432a7
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-26 22:13:02 +0800

    migrations added

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from database.models import db
 from database.tools import generate_uri_from_file
 
@@ -11,5 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+migrate = Migrate(app, db)
 
 from app import routes
