37769c8c3f1089ea6a6bafd3a7f2316c3917ee8d
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-26 22:19:51 +0800

    read from posts db

--- app/routes/feed.py
+++ app/routes/feed.py
@@ -1,6 +1,20 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request, redirect, url_for
+from database.models import Posts
 
-@app.route('/feed')
+@app.route('/feed', methods=['GET', 'POST'])
 def feed():
-    return render_template('feed.html')
+
+    if request.method == 'POST':
+        desc = request.form.get('desc')
+        
+        newPost = Posts(desc=desc)
+        db.session.add(newPost)
+        db.session.commit()
+
+        return redirect(url_for('feed'))
+    
+    else:
+        posts = Posts.query.all()
+
+        return render_template('feed.html', posts=posts)
