138fc4002963309bd23d56a595bedf9318f96f6e
ReeveNTH <112465682+ReeveNTH@users.noreply.github.com>
2023-05-27 23:50:12 +0800

    MORE EVENTS

---
+++ app/static/scripts/events.js
@@ -0,0 +1,30 @@
+const events = [
+    { name: "rooftop gardening at tiong bahru"},
+    { name: "beach cleanup at east coast park"},
+    { name: "environmental workshop"},
+    { name: "save the turtles"},
+    { name: "recycling workshop"},
+    { name: "tree planting at yishun"},
+
+
+]
+
+const searchInput = document.getElementbyId("searchnput");
+
+const namesFromDom = document.getElementsbyClassName("title");
+
+searchInput.addEventListener("keyup", (event) =>) {
+    const { value } = event.target;
+
+    const searchQuery = value.toLowerCase();
+
+    for (const nameElement of namesFromDom) {
+        let name = nameElement.textContent.toLowerCase();
+
+        if (name.includes(searchQuery)) {
+           nameElement.stype.display = "block";
+        } else{
+          nameElement.stype.display = "none";
+        }
+    }
+}
