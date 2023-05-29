26d599391cb5be5fcbf9a27892218fdc2c43dd8e
cheongsojumhaji <122714406+cheongsojumhaji@users.noreply.github.com>
2023-05-29 23:10:39 +0800

    some global leaderboard ui

--- app/static/scripts/leaderboard.js
+++ app/static/scripts/leaderboard.js
@@ -19,4 +19,26 @@ function openTab(evt, LeaderboardType) {
   evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
+function openGlobal(evt, GlobalType) {
+  // Declare all variables
+  var i, globalcontent, globallinks;
+
+  // Get all elements with class="globalcontent" and hide them
+  globalcontent = document.getElementsByClassName("globalcontent");
+  for (i = 0; i < globalcontent.length; i++) {
+    globalcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="globallinks" and remove the class "active"
+  globallinks = document.getElementsByClassName("globallinks");
+  for (i = 0; i < globallinks.length; i++) {
+    globallinks[i].className = globallinks[i].className.replace(" active", "");
+  }
+
+  // Show the current global, and add an "active" class to the button that opened the global
+  document.getElementById(GlobalType).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultGlobalOpen").click();
+document.getElementById("defaultAllTimeOpen").click();
