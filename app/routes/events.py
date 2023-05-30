1e2f50e0f000e018962e633cd4ce0bab86d1a0a1
ReeveNTH <reeve.nth@gmail.com>
2023-05-30 19:47:01 +0800

    events yes

--- app/routes/events.py
+++ app/routes/events.py
@@ -12,20 +12,24 @@ def events():
 #NAVIN CODE
 @app.route('/events/add', methods=["GET","POST"])
 def add_events():
-    if FormEvents.validate() and request.method == "POST":
-        # picture_1 = save_image(request.files.get('picture_1'), request.files.get('picture_1').filename)
-        event = Events(name=FormEvents.name.data.title(), date=FormEvents.date.data, time=FormEvents.time.data,
-                           price=FormEvents.price.data, organiser=FormEvents.organiser.data)
-                            # picture_1=picture_1.filename)
-        db.session.add(event)
-        db.session.commit()
-        db.session.close()
 
-
-        return render_template('addevents.html', form=FormEvents)
+    form = FormEvents(request.form)
+    # if form.validate() and request.method == "POST":
+    #     # picture_1 = save_image(request.files.get('picture_1'), request.files.get('picture_1').filename)
+    #     event = Events(name=FormEvents.name.data.title(), date=FormEvents.date.data, time=FormEvents.time.data,
+    #                        price=FormEvents.price.data, organiser=FormEvents.organiser.data)
+    #                         # picture_1=picture_1.filename)
+    #     db.session.add(event)
+    #     db.session.commit()
+    #     db.session.close()
+    #
+    #
+    #     return redirect(url_for('events'))
     # else:
     #     flash('You Are Not Authorised to View the Employee Portal', 'danger')
     #     return abort(403)
     # else:
     #     loginmanager.login_message_category = 'warning'
     #     return app.login_manager.unauthorized()
+    # else:
+    return render_template('addevents.html', form=form)
