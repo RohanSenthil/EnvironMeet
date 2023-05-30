f84f5fefdb5dd80f2ac170cf708419e1671a24bb
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-31 00:16:05 +0800

    preview img

--- app/static/scripts/feed.js
+++ app/static/scripts/feed.js
@@ -2,6 +2,8 @@
 const newPostInput = document.getElementById('newPostInput');
 const charCounter = document.getElementById('charCounter');
 const commentInput = document.getElementById('commentInput');
+const addImageForPost = document.getElementById('addImageForPost');
+const previewImageHolder = document.getElementById('previewImage');
 
 newPostInput.style.height = `height: ${newPostInput.scrollHeight}`;
 
@@ -11,6 +13,47 @@ const charLimit = (limitField, limitNum) => {
         limitField.value = limitField.value.substring(0, limitNum)
         charCounter.innerText = 0;
     }
+}
+
+const removePreview = (target) => {
+    while (target.hasChildNodes()) {
+        target.removeChild(target.firstChild);
+    }
+}
+
+const previewImage = (src, target) => {
+
+    // Reset
+    removePreview(target)
+
+    // Preview
+    file = src.files[0]
+
+    if (file) {
+        const fileRead = new FileReader();
+        fileRead.readAsDataURL(file);
+        fileRead.addEventListener('load', () => {
+            let img = document.createElement('img');
+            let closeIcon = document.createElement('span');
+            let btn = document.createElement('button');
+
+            img.src = fileRead.result;
+
+            closeIcon.classList.add('closeIcon');
+
+            btn.setAttribute('type', 'button');
+            btn.addEventListener('click', () => {
+                removePreview(previewImageHolder)
+            });
+            btn.appendChild(closeIcon)
+
+            img.src = fileRead.result;
+
+            target.appendChild(img);
+            target.appendChild(btn);
+        })
+    }
+    
 }
 
 newPostInput.addEventListener('input', (e) => {
@@ -31,4 +74,8 @@ commentInput.addEventListener('input', (e) => {
     maxChars = 250;
 
     charLimit(e.target, maxChars);
+})
+
+addImageForPost.addEventListener('input', (e) => {
+    previewImage(e.target, previewImageHolder)
 })
