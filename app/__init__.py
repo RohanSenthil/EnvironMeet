2b640c83cd0b80b4e98e2f19b016b114045d242c
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-30 21:18:55 +0800

    added secret key config

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_migrate import Migrate
 from database.models import db
 from database.tools import generate_uri_from_file
+import os
 
 app = Flask(__name__)
 
 db_uri = generate_uri_from_file('database/db_config.yml')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 # For Testing
 app.config['TEMPLATES_AUTO_RELOAD'] = True
