d8cf935b59bbe9be50748f0b89385432680c766b
xavierwoon-cmd <111954471+xavierwoon-cmd@users.noreply.github.com>
2023-05-30 16:30:43 +0800

    trying register LOL

---
+++ app/forms/accountsform.py
@@ -0,0 +1,12 @@
+from wtforms import Form, StringField, SelectField, TextAreaField, validators, IntegerField , FileField , FloatField, TimeField, PasswordField
+from wtforms.validators import ValidationError, InputRequired,DataRequired, EqualTo, Email
+from flask_wtf.file import FileRequired , FileAllowed, FileField
+from wtforms.fields import DateField
+
+class registerm(Form):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    gender = SelectField('Gender', choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')], validators=[DataRequired()])
+    contact = StringField('Contact', validators=[DataRequired()])
