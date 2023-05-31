794499a29053b24532f04c0337ce14d8b56736cb
RohanSenthil <rohansenthilrs03@gmail.com>
2023-05-31 11:11:32 +0800

    delete post func

--- app/routes/feed.py
+++ app/routes/feed.py
@@ -5,36 +5,56 @@ from app.forms.feedForms import PostForm
 from werkzeug.utils import secure_filename
 import os
 
-@app.route('/feed', methods=['GET', 'POST'])
+@app.route('/feed', methods=['GET'])
 def feed():
     newPostForm = PostForm()
-    if request.method == 'POST':
+    posts = Posts.query.all()
 
-        if newPostForm.validate_on_submit():
-
-            newPost = Posts(desc=newPostForm.desc.data)
-
-            # Handling file upload
-            uploaded_file = newPostForm.image_1.data
-
-            if uploaded_file is not None:
-                filename = secure_filename(uploaded_file.filename)
-                image_path = os.path.join(app.config['UPLOAD_PATH'], filename)
-                uploaded_file.save(image_path)
-                newPost.image = image_path
-                path_list = newPost.image.split('/')[1:]
-                new_path = '/'.join(path_list)
-                
-                newPost.image = new_path
-
-            db.session.add(newPost)
-            db.session.commit()
-
-            flash('Post created!')
-
-        return redirect(url_for('feed'))
+    return render_template('feed.html', posts=posts, newPostForm=newPostForm)
     
-    else:
-        posts = Posts.query.all()
 
-        return render_template('feed.html', posts=posts, newPostForm=newPostForm)
+@app.route('/post/create', methods=['POST'])
+def createPost():
+    newPostForm = PostForm()
+    if request.method == 'POST' and newPostForm.validate_on_submit():
+
+        newPost = Posts(desc=newPostForm.desc.data)
+
+        # Handling file upload
+        uploaded_file = newPostForm.image_1.data
+
+        if uploaded_file is not None:
+            filename = secure_filename(uploaded_file.filename)
+            image_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+            uploaded_file.save(image_path)
+            newPost.image = image_path
+            path_list = newPost.image.split('/')[1:]
+            new_path = '/'.join(path_list)
+            
+            newPost.image = new_path
+
+        db.session.add(newPost)
+        db.session.commit()
+
+    return redirect(url_for('feed'))
+
+
+@app.route('/post/edit/<int:postid>', methods=['POST'])
+def editPost(postid):
+
+    # Implement Authorisation Check
+    return redirect(url_for('feed'))
+
+
+@app.route('/post/delete/<int:postid>', methods=['POST'])
+def deletePost(postid):
+
+    # Implement Authorisation Check
+
+    post = Posts.query.get(postid)
+
+    if post is not None:
+        db.session.delete(post)
+        db.session.commit()
+
+    return redirect(url_for('feed'))
