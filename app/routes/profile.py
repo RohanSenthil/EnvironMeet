f1ea79a734d00de156a3910f3f36ae6aebd0ac54
xavierwoon-cmd <111954471+xavierwoon-cmd@users.noreply.github.com>
2023-05-31 10:56:45 +0800

    why 404 bruh

---
+++ app/routes/profile.py
@@ -0,0 +1,18 @@
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+# from app.routes.helpers import privileged_route
+from flask_mail import Message
+from threading import Thread
+from flask import request, render_template, redirect, url_for, flash
+from app import app
+from database.models import Members, Organisations, db
+from app.forms.accountsform import createm, updatem
+
+
+@app.route('/profile')
+# @login_required
+def profile():
+    # if not current_user.is_authenticated:
+        # loginmanager.login_message = "Please login to access this page"
+        # loginmanager.login_message_category = "warning"
+    return render_template('/accounts/profile/memprofile.html') #, name=current_user.name
