b1b431b5eebe4f3f23424da99185b4def63a08dd
ReeveNTH <reeve.nth@gmail.com>
2023-07-02 20:18:12 +0800

    Minor changes

--- app/routes/attendance.py
+++ app/routes/attendance.py
@@ -1,10 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-from database.models import Attendance
+from database.models import Attendance, SignUps
 from app.util import id_mappings
 
 @app.route('/attendance')
 def display_attendance():
-    attendance_records = Attendance.query.all()
+    # attendance_records = Attendance.query.all()
+    eventsignups = SignUps.query.all()
 
-    return render_template('attendance.html', records=attendance_records, get_event_from_id=id_mappings.get_event_from_id)
+    return render_template('attendance.html', get_event_from_id=id_mappings.get_event_from_id, records=eventsignups)
