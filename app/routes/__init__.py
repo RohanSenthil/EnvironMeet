8045597af254c1ac5ab5db2b12c7aa65c3aa69e0
RohanSenthil <rohansenthilrs03@gmail.com>
2023-08-03 22:54:49 +0800

    test route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,15 @@
 from app import app
 from flask import render_template
 from app.routes import feed, events, accounts, leaderboard, profile, helpers, attendance, report, chat
+from flask.json import jsonify
 
 # Routes
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Test Route
+@app.route('/critical')
+def raise_critical():
+    app.logger.critical(f'Error: This is a test', extra={'security_relevant': False, 'http_status_code': 500})
+    return jsonify({'logged': 'critical error test'}, 500)
