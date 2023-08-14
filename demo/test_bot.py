77f96de69ae83f294c25c4ca2c598f8ebc124816
RohanSenthil <rohansenthilrs03@gmail.com>
2023-08-15 01:43:11 +0800

    remove demo script

--- demo/test_bot.py
+++
@@ -1,9 +0,0 @@
-import requests
-import webbrowser
-import time
-
-BASE_URL = 'http://127.0.0.1:5000/'
-HEADERS = {'User-Agent': 'TestBot'}
-
-def open_browser(url):
-    webbrowser.open(url, new=2)
